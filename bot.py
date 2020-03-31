ee397cdac3b3926fa38de73fe48a4c844b9e07d4
fenrir4ik <46963294+fenrir4ik@users.noreply.github.com>
2020-03-31 18:55:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot("1093407058:AAEmG84mCBHEVX3Mt1raN4xDN94LOcB913Y")
 
 @bot.message_handler(commands=['cmd'])
 def send_welcome(message):
-    bot.reply_to(message, "Бот жив ёпта!")
+    bot.reply_to(message, "Реализуется потом")
 
 
 bot.polling()
