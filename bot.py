51e4994d0b26066a8484b6d4a65a58109905485c
fenrir4ik <46963294+fenrir4ik@users.noreply.github.com>
2020-03-31 21:04:08 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,14 +4,18 @@ import telebot
 
 bot = telebot.TeleBot("1093407058:AAEmG84mCBHEVX3Mt1raN4xDN94LOcB913Y")
 
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Напиши мне 'Привет'")
 
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
-    bot.reply_to(message, "Я юзлесс, но напишите 'Привет'")
+    bot.reply_to(message, "Я юзлесс, на привет отвечу")
 
 @bot.message_handler(content_types=['text'])
 def msg(message):
     if (message.text == "Привет" or message.text == "привет"):
         bot.send_photo(message.chat.id, open('1.jpg', 'rb'))
+        bot.reply_to(message, "А теперь пока, потому-что я больше ничего не умею")
 
 bot.polling()
